f3ef1473477cd50c3bac605548ceb2e875abb432
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-06 00:14:59 -0400

    Add files via upload

---
+++ 01_getting_started/01_01_python_org.py
@@ -0,0 +1,9 @@
+# My first ever code!
+
+'''
+In Python.org, I was intereted in the "Community" section which would allow me to connect with other Python enthusiasts.
+Moreover, the "Success Stories" and "News" sections keep me updated on the language’s applications and developments.
+It is a very useful tool for all levels of coders. I can't wait to use it.
+'''
+
+print("Hello World!")
