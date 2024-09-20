0e54504ed0f6d9802df7acc8a6b6e5115406ab40
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-19 23:25:00 -0400

    Add files via upload

---
+++ 04_01_pizzas.py
@@ -0,0 +1,12 @@
+
+# Print for loop:
+pizzas = ['Cheese pizza', 'Buffalo chicken pizza', 'Chicken fajita pizza']
+for pizza in pizzas:
+    print(pizza)
+
+# Modify you for loop to print a message for every element in the list.
+for pizza in pizzas:
+    print(f"\n I like {pizza.title()}.\n")
+
+# Add line outdide of the for loop.
+print("\nI really love pizza!\n")
