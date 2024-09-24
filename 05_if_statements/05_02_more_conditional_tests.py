7140a3d5e468e3a0ce8d6953b24dd82eb70d3993
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-24 14:43:28 +0000

    week 5

--- 05_if_statements/05_02_more_conditional_tests.py
+++ 05_if_statements/05_02_more_conditional_tests.py
@@ -7,3 +7,18 @@ print(car.lower() == 'bmw')
 print("\nI think my dad's favorite car is bmw.")
 print(car == 'bmw')
 
+# Test using equality:
+magic_number = 27
+print("\nThe magic number is 27.")
+print(magic_number == 27)
+
+print("\nThe magic number is 7.")
+print(magic_number == 7)
+
+# Test using inequality:
+lucky_number = 7
+print("\nThe lucky number is not 5.")
+print(lucky_number != 5)
+
+print("\nThe lucky number is 5.")
+print(lucky_number == 5)
