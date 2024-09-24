97fee33b605dd0d5b17922dff9eb963c6388624a
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-24 14:15:54 +0000

    week 5

---
+++ 05_if_statements/05_02_more_conditional_tests.py
@@ -0,0 +1,9 @@
+
+# Test using the lower() method:
+car = 'BMW'
+print("\nI think my dad's favorite car is bmw.")
+print(car.lower() == 'bmw')
+
+print("\nI think my dad's favorite car is bmw.")
+print(car == 'bmw')
+
