3a40f18e6c34e5174d92437045758f21478748ab
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-24 16:15:59 -0400

    Add files via upload

---
+++ 05_if_statements/05_01_conditional_tests.py
@@ -0,0 +1,40 @@
+
+# Test 1:
+fruit = 'strawberry'
+print("\nIs fruit == 'stawberry'? I predict True.")
+print(fruit == 'strawberry')
+
+print("\nIs fruit == 'pineapple'? I predict False.")
+print(fruit == 'pineapple')
+
+# Test 2:
+age = 18
+print("\nIs your age <= 19? I predict True.")
+print(age <= 19)
+
+print("\nIs age > 20? I predict False.")
+print(age > 20)
+
+# Test 3:
+favorite_car = 'ferrari'
+print("\nIs your favorite_car != 'audi'? I predict True.")
+print(favorite_car != 'audi')
+
+print("\nIs your favorite_car == 'bmw'? I predict False.")
+print(favorite_car == 'bmw')
+
+# Test 4:
+nationality = 'indian'
+print("\nIs your nationality == 'indian'? I predict True.")
+print(nationality == 'indian')
+
+print("\nIs your nationality == 'cuban'? I predict False.")
+print(nationality == 'cuban')
+
+# Test 5:
+city_of_love = 'paris'
+print("\nIs the city_of_love == 'paris'? I predict True.")
+print(city_of_love == 'paris')
+
+print("\nIs the city_of_love == 'venice'? I predict False.")
+print(city_of_love == 'venice')
