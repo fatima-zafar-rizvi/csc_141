b151b472e8e78beff1f435063b74b32b007c6549
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-09-30 08:03:16 -0400

    Update 05_13_your_ideas.py

---
+++ 05_if_statements/05_13_your_ideas.py
@@ -0,0 +1,16 @@
+# Problems I want to tackle as I continue to improve:
+
+'''
+I want to explore more data sets going forward. Similarly, I would like to 
+build an app where users can upload their data and visualize them with graphs 
+and charts.
+
+I have experience working as a multi-media manager for a business. 
+Therefore, I am curious about how social media can be used to determine  
+public sentiment about specific topics. I think this will be useful for 
+businesses to gauge their audience with the help of natural language processing.
+
+As for an immediate goal, I want to create a personal finance tracker for 
+myself as a side project. I would like to see how my economics background
+can merge with my coding skills in Python.
+'''
