43b72e151484eb76fc11a94236ac1d2f588a07f0
fatima-zafar-rizvi <fatima.rizvi001@albright.edu>
2024-10-04 20:19:02 -0400

    Add files via upload

---
+++ 06_01_person.py
@@ -0,0 +1,9 @@
+# Using dictionary to store information about a person:
+spiderman = {'first_name': 'peter', 'last_name': 'parker', 'age': '19', 
+             'city': 'nyc'}
+
+# Printing the information stored in the dictoionary:
+print(spiderman['first_name'])
+print(spiderman['last_name'])
+print(spiderman['age'])
+print(spiderman['city'])
